bd404e1658a06178e58b642cb5ee579db4beb17b
MOBLEY <MOBLEY@DESKTOP-TRS0JM0>
2019-05-06 23:01:52 -0300

    Atualizando entidade usuario.

--- src/model/entity/User.java
+++ src/model/entity/User.java
@@ -3,14 +3,20 @@ package model.entity;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name="USER")
 public class User {
 	
 	@Id
+	@Column(name = "ID_USUARIO")
 	private Long id;
 	
-	@Column
-	private String name;
+	@Column(name = "LOGIN")
+	private String login;
+	
+	@Column(name = "SENHA")
+	private String senha;
 	
 }
