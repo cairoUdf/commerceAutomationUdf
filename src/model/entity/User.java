04f82fdcd20963a4c905ed569d7a210abab109f8
MOBLEY <MOBLEY@DESKTOP-TRS0JM0>
2019-05-06 22:57:06 -0300

    Atualização.

--- src/model/entity/User.java
+++ src/model/entity/User.java
@@ -1,5 +1,16 @@
 package model.entity;
 
-public class User {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
+@Entity
+public class User {
+	
+	@Id
+	private Long id;
+	
+	@Column
+	private String name;
+	
 }
